bc71d4e0d265dedb9f6cc2e62f88feb2807afe1b
Alex Viedma <ajviedma21@blakeschool.org>
2018-09-07 13:51:37 -0500

    Camera Motion and lighting changes

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            transform.Translate(Vector3.forward);
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.Translate(Vector3.left);
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.Translate(Vector3.right);
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.Translate(Vector3.back);
+        if (Input.GetKeyDown(KeyCode.E))
+            transform.Translate(Vector3.up);
+        if (Input.GetKeyDown(KeyCode.Q))
+            transform.Translate(Vector3.down);
+    }
+}
