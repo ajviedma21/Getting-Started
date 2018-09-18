7096854b127242bb5a0939707d4ea0185a559a44
Alex Viedma <ajviedma21@blakeschool.org>
2018-09-17 19:03:09 -0500

    Final

--- Assets/Scripts/CamMove.cs
+++ Assets/Scripts/CamMove.cs
@@ -17,9 +17,9 @@ public class CamMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
             transform.Translate(Vector3.forward);
         if (Input.GetKeyDown(KeyCode.A))
-            transform.Translate(Vector3.left);
-        if (Input.GetKeyDown(KeyCode.D))
             transform.Translate(Vector3.right);
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.Translate(Vector3.left);
         if (Input.GetKeyDown(KeyCode.S))
             transform.Translate(Vector3.back);
         if (Input.GetKeyDown(KeyCode.E))
@@ -28,4 +28,3 @@ public class CamMove : MonoBehaviour
             transform.Translate(Vector3.down);
     }
 }
-//final
